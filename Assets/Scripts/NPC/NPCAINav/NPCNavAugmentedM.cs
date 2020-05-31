65ec28661e9308568d916528467195b271dbddbb
Edgard Thobias <edgardsoares_ed@hotmail.com>
2020-05-31 01:01:03 -0400

    Updates
    - Foi adicionando um novo NPC para spawnar e andar pelo mapa: A junky Female

---
+++ Assets/Scripts/NPC/NPCAINav/NPCNavAugmentedM.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.AI;
+using UnityEngine.AI;
+
+public class NPCNavAugmentedM : MonoBehaviour
+{
+    public GameObject _destination;
+    NavMeshAgent _theAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _theAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _theAgent.SetDestination(_destination.transform.position);
+
+        if(transform.position.z >= 20)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/NPC/NPCAINav/NPCNavJunkyF.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCNavJunkyF : MonoBehaviour
+{
+    public GameObject _destination;
+    NavMeshAgent _theAgent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _theAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _theAgent.SetDestination(_destination.transform.position);
+
+        //Destroy Object
+        if(transform.position.z >= 26)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+
+}
