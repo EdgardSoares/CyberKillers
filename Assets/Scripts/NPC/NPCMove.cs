80ad7bf8c0e3c67d43e7a6294707800204200249
Edgard Thobias <edgardsoares_ed@hotmail.com>
2020-05-30 21:58:19 -0400

    Updates

---
+++ Assets/Scripts/NPC/NPCMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMove : MonoBehaviour
+{
+
+    private float _speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 _pos = transform.position;
+        _pos.z = _pos.z + _speed * Time.deltaTime;
+        transform.position = _pos;
+        
+    }
+}
